5791cf2bcd8be69979dca4a4276b8599a44ae69d
Daniyal Aamir <daamir89@gmail.com>
2024-06-19 12:34:45 -0400

    implement stats component individual sections

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -1,10 +1,9 @@
 import { useState } from "react"
 import Warning from "./Warning"
 
-export default function Textarea() {
-  const [text, setText] = useState('')
+export default function Textarea({ text, setText }) {
   const [warningText, setWarningText] = useState('')
-
+  
   const handleChange = (e) => {
     let newText = e.target.value
     if(newText.includes('<script>')) {
